b40ee9bf2bc3914f1cb278b7ca7a6b0b3782bf24
Ayomipo Majek <ayomipomajek@gmail.com>
2023-03-09 22:50:38 +0100

    mission test done

---
+++ src/store/mission/tests/missionSlice.test.js
@@ -0,0 +1,34 @@
+import { configureStore } from '@reduxjs/toolkit';
+import missionReducer, { fetchMissionData, joinMission, leaveMission } from './missionSlice';
+
+describe('missionSlice', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({
+      reducer: {
+        mission: missionReducer,
+      },
+    });
+  });
+
+  it('should fetch mission data successfully', async () => {
+    await store.dispatch(fetchMissionData());
+    const missions = store.getState().mission.mission;
+    expect(missions.length).toBeGreaterThan(0);
+  });
+
+  it('should join a mission successfully', () => {
+    const missionId = 1; // Replace with a valid mission ID
+    store.dispatch(joinMission(missionId));
+    const joinedMission = store.getState().mission.mission.find((mission) => mission.id === missionId);
+    expect(joinedMission.joined).toBe(true);
+  });
+
+  it('should leave a mission successfully', () => {
+    const missionId = 1; // Replace with a valid mission ID
+    store.dispatch(leaveMission(missionId));
+    const leftMission = store.getState().mission.mission.find((mission) => mission.id === missionId);
+    expect(leftMission.joined).toBe(false);
+  });
+});
