96e81cf8f14a915e109b41142ad001e0e22f0d53
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-03-09 10:46:58 -0400

    Add profile page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Rocket from './pages/Rocket/Rocket';
-import Mission from './pages/Mission/Mission';
+import { Rocket, Mission, MyProfile } from './pages';
 
 function App() {
   return (
@@ -11,6 +10,7 @@ function App() {
         <Route index element={<Rocket />} />
         <Route index path="/Rocket" element={<Rocket />} />
         <Route path="/Mission" element={<Mission />} />
+        <Route path="/Profile" element={<MyProfile />} />
       </Route>
     </Routes>
   );
